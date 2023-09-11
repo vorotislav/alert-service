a7e69fbbddb4636d7c6a80c3a8cfe3fb6e20a6e8
vorotislav <azovskiyv@gmail.com>
2023-09-11 16:20:26 +0500

    add env

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -1,11 +1,25 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"github.com/caarlos0/env/v6"
+)
 
 var flagRunAddr string
+
+type Config struct {
+	Address string `env:"ADDRESS"`
+}
 
 func parseFlag() {
 	flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
 
 	flag.Parse()
+
+	cfg := Config{}
+	if err := env.Parse(&cfg); err == nil {
+		if cfg.Address != "" {
+			flagRunAddr = cfg.Address
+		}
+	}
 }
