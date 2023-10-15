8d28227584cb6f3aac0dcf27aabb2f50badfc746
vorotislav <azovskiyv@gmail.com>
2023-10-16 01:36:23 +0500

    fix model tag

--- internal/model/model.go
+++ internal/model/model.go
@@ -2,7 +2,7 @@ package model
 
 type Metrics struct {
 	ID    string   `json:"id"`
-	MType string   `json:"type"`
+	MType string   `json:"mtype"`
 	Delta *int64   `json:"delta,omitempty"`
 	Value *float64 `json:"value,omitempty"`
 }
